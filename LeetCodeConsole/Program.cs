be3e98b8a28025f8a184198c3053d35ee6ae2e8a
mdsayed83 <mdsayed83@gamil.com>
2022-08-26 15:15:26 +0600

    Add project files.

---
+++ LeetCodeConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeConsole
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            var result = RomanToInt("IXL");
+            int RomanToInt(string s)
+            {
+                var result = 0;
+                var dictionary = new Dictionary<char, int>
+            {
+                {'I',1 },
+                {'V',5 },
+                {'X',10 },
+                {'L',50 },
+                {'C',100 },
+                {'D',500 },
+                {'M',1000 },
+            };
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        result += dictionary[s[i]];
+                        continue;
+                    }
+                    if (dictionary[s[i - 1]] >= dictionary[s[i]])
+                    {
+                        result += dictionary[s[i]];
+                    }
+                    else
+                    {
+                        result = result + dictionary[s[i]] - (2 * dictionary[s[i - 1]]);
+                    }
+                }
+                return result;
+            }
+            Console.WriteLine(result);
+        }
+
+        
+    }
+}
